cd91db32491a76ac46085b38a3ebfcd09ebf5d59
Matt Thrasher <matthew.j.thrasher@gmail.com>
2022-03-26 11:53:23 -0500

    update state with user input

--- src/Input.js
+++ src/Input.js
@@ -1,7 +1,23 @@
 import React from 'react';
 
-const Input = (props) => {
-  return <div data-test='component-input'></div>;
+const Input = ({ secretWord }) => {
+  const [currentGuess, setCurrentGuess] = React.useState(''); // this way lets us mock useState
+  return (
+    <div data-test='component-input'>
+      <form className='form-inline'>
+        <input
+          data-test='input-box'
+          className='mb-2 mx-sm-3'
+          placeholder='enter guess'
+          value={currentGuess}
+          onChange={(event) => setCurrentGuess(event.target.value)}
+        />
+        <button data-test='submit-button' className='btn btn-primary mb-2'>
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Input;
